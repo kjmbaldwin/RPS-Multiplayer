152ecaa97fd1c684692a33f5258c4a29505b0115
Kyle Baldwin <kylejmbaldwin@gmail.com>
2017-12-03 16:20:44 -0500

    Setup bootstrap, and firebase loads player 1 and 2

--- assets/logic.js
+++ assets/logic.js
@@ -11,3 +11,60 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+
+$('#submit-btn').on('click', function(event){ 
+  event.preventDefault();
+
+  var userName = $('#user-name').val().trim();
+  console.log(userName);
+
+
+  database.ref().push({
+    player: userName,
+  })
+
+  $('#user-name').val('');
+
+
+});
+
+
+database.ref().on('child_added', function(childSnapshot){ 
+
+  var player = childSnapshot.val().player;
+
+  //if player 1 class = empty, then append palyer 1 and change class to full
+  if ($('#player1').hasClass('empty')){
+     
+    $('#player1').removeClass('empty').addClass('full');
+
+    var $rock = $('<div>').addClass('guess').attr('id', 'p1-rock').text('Rock');
+    var $paper = $('<div>').addClass('guess').attr('id', 'p1-paper').text('Paper');
+    var $scissors = $('<div>').addClass('guess').attr('id', 'p1-scissors').text('Scissors');
+
+    $('#player1').append(player, $rock, $paper, $scissors); 
+
+
+  } else if ($('#player2').hasClass('empty')){
+
+    $('#player2').removeClass('empty').addClass('full');
+
+    var $rock = $('<div>').addClass('guess').attr('id', 'p2-rock').text('Rock');
+    var $paper = $('<div>').addClass('guess').attr('id', 'p2-paper').text('Paper');
+    var $scissors = $('<div>').addClass('guess').attr('id', 'p2-scissors').text('Scissors');
+
+    $('#player2').append(player, $rock, $paper, $scissors); 
+  }
+
+
+  
+  //else if same for player 2
+
+
+});
+
+
+//onclick send user name to firebase
+//initialize wins and loses 
+//create buttons for r p and s
